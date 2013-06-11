df5f9119e7a0201c93ca2bbb80e3772f7c3be592
Mariusz Smykuła <mariuszs@gmail.com>
2013-06-11 22:16:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+## Starting cassandra and web app
+
+    mvn cassandra:start jetty:run -Dcassandra.jmxPort=7199
+
+## Loading `load.script`
+
+    mvn cassandra:load
+
+
+# Links
+
+http://mojo.codehaus.org/cassandra-maven-plugin/index.html
+
---
+++ src/main/java/no/force/cassandra/config/NoForceConfig.java
@@ -0,0 +1,10 @@
+package no.force.cassandra.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("no.force.cassandra")
+public class NoForceConfig {
+
+}
